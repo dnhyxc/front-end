4b583e5f31642828b00500fb300318669d0c36c3
dengnh <dengninghai@shinemo.com>
2024-01-05 15:07:40 +0800

    electron

---
+++ vue/electron/electron-vue3/app/main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  mainWindow.loadURL("http://localhost:5173/");
+
+  mainWindow.webContents.openDevTools();
+  // mainWindow.on();
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
---
+++ vue/electron/electron-vue3/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <iframe
+      src="/public/test.pdf"
+      width="100%"
+      height="100%"
+      frameborder="0"
+    ></iframe>
+  </div>
+</template>
+
+<style scoped></style>
