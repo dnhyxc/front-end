72e2efc25d90570a81cd16e09d96e1517c07e4aa
dnhyxc <2640559435@qq.com>
2024-04-08 19:28:01 +0800

    feat: update

--- vue/vue3-lamejs/src/App.vue
+++ vue/vue3-lamejs/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-    <Home />
+    <Home/>
+    <Emoji/>
   </div>
 </template>
 
 <script setup lang="ts">
 import Home from "../src/views/home/index.vue";
+import Emoji from "../src/views/emoji/index.vue";
 </script>
