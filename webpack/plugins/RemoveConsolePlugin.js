c021858a1c449c3978a8611e9d9fabc937dcde6b
dnhyxc <2640559435@qq.com>
2024-10-18 10:29:58 +0800

    feat: 增加console移除插件

--- webpack/plugins/RemoveConsolePlugin.js
+++ webpack/plugins/RemoveConsolePlugin.js
@@ -1,116 +1,47 @@
-const parser = require("@babel/parser");
-const traverse = require("@babel/traverse").default;
-const generate = require("@babel/generator").default;
-
-const code = `
-  console.log('This is a log');
-  const x = 5;
-  console.log(x);
-`;
-
-const ast = parser.parse(code);
-
-// 遍历 AST 并删除 console.log 调用
-traverse(ast, {
-  CallExpression(path) {
-    // 检查是否是 console.log
-    if (
-      path.node.callee.type === "MemberExpression" &&
-      path.node.callee.object.name === "console" &&
-      path.node.callee.property.name === "log"
-    ) {
-      path.remove(); // 删除该节点
-    }
-  }
-});
-
-// 生成修改后的代码
-const { code: newCode } = generate(ast);
-
-console.log(newCode);
-
-// RemoveConsolePlugin.js
-// class RemoveConsolePlugin {
-//   // 由于我们不需要从外部传入 options 
-//   // 因此这里就不显示地定义 constructor 了
-//   // constructor (){...}
-
-//   apply(compiler) {
-//     compiler.hooks.emit.tapAsync(
-//       "RemoveConsolePlugin",
-//       (compilation, callback) => {
-//         Object.keys(compilation.assets).forEach((filename) => {
-//           // 仅处理 .js 文件
-//           if (filename.endsWith(".js")) {
-//             const asset = compilation.assets[filename];
-//             let content = asset.source();
-
-//             // 使用正则表达式移除整个 console.log 语句
-//             // 匹配 console.log( 之后的任意字符，直到遇到闭合的括号
-//             const consoleLogRegex = new RegExp(
-//               "console\\.log\\(.*?\\)(,|$)",
-//               "g"
-//             );
-
-//             const withoutConsole = content.replace(consoleLogRegex, "");
-
-//             // 更新资源
-//             compilation.assets[filename] = {
-//               source: () => withoutConsole,
-//               size: () => Buffer.byteLength(withoutConsole, "utf8"),
-//             };
-//           }
-//         });
-
-//         callback();
-//       }
-//     );
-//   }
-// }
-
-// module.exports = RemoveConsolePlugin;
-
 const { transform } = require('@babel/core');
 
-class RemoveConsoleLogPlugin {
+class RemoveConsolePlugin {
   apply(compiler) {
-    compiler.hooks.emit.tapAsync('RemoveConsoleLogPlugin', (compilation, callback) => {
+    console.log(compiler.hooks.emit, 'compiler.hooks.emit')
+    compiler.hooks.emit.tapAsync('RemoveConsolePlugin', (compilation, callback) => {
       // 遍历每个模块
       for (const filename in compilation.assets) {
         const asset = compilation.assets[filename];
         const source = asset.source();
-
-        // 使用 Babel 转换源代码，删除 console.log
-        const result = transform(source, {
-          plugins: [
-            function removeConsoleLog() {
-              return {
+        if (filename.endsWith('.js')) {
+          const result = transform(source, {
+            // 这是一个用于移除控制台输出的插件
+            plugins: [
+              {
+                name: 'remove-console',
                 visitor: {
                   CallExpression(path) {
-                    if (
-                      path.node.callee.type === "MemberExpression" &&
-                      path.node.callee.object.name === "console" &&
-                      path.node.callee.property.name === "log"
-                    ) {
+                    // 如果调用表达式的调用者是console对象，就移除这个调用表达式
+                    if (path.node.callee.type === 'MemberExpression' && path.node.callee.object.name === 'console') {
                       path.remove();
                     }
-                  }
-                }
-              };
-            }
-          ]
-        });
-
-        // 更新模块的源代码
-        compilation.assets[filename] = {
-          source: () => result.code,
-          size: () => result.code.length,
-        };
+                  },
+                  ConditionalExpression(path) {
+                    // 如果条件表达式的结果是console对象的调用，我们将其替换为undefined，以移除控制台输出
+                    if (path.node.consequent.type === 'CallExpression' && path.node.consequent.callee.type === 'MemberExpression' && path.node.consequent.callee.object.name === 'console') {
+                      path.replaceWithSourceString('undefined');
+                    }
+                  },
+                },
+              },
+            ],
+            ast: true, // 确保生成 AST
+            code: true, // 确保生成代码
+          });
+          compilation.assets[filename] = {
+            source: () => result.code,
+            size: () => result.code.length,
+          };
+        }
       }
-
       callback();
     });
   }
 }
 
-module.exports = RemoveConsoleLogPlugin;
+module.exports = RemoveConsolePlugin;
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -5,6 +5,9 @@ const ResourceAnalyzerPlugin = require('./plugins/ResourceAnalyzerPlugin');
 const CustomOutputPlugin = require('./plugins/CustomOutputPlugin');
 const CodeInjectionPlugin = require('./plugins/CodeInjectionPlugin');
 const ConsoleLogPlugin = require('./plugins/ConsoleLogPlugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const RemoveConsolePlugin = require('./plugins/RemoveConsolePlugin');
+const webpackDevServer = require('webpack-dev-server');
 
 module.exports = {
   entry: './src/index.js',
@@ -21,8 +24,29 @@ module.exports = {
     //   injectedCode: `const customFn = (a, b) => a * b;console.log(customFn(2, 9));\n`
     // }),
     // new ChangeImportPlugin({useSub: process.env.USE_SUB === 'true'}),
-    new ConsoleLogPlugin()
+    // new ConsoleLogPlugin()
+    new RemoveConsolePlugin(),
+    // 配置 HTML 插件
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+      inject: 'body',
+      minify: {
+        removeComments: true,
+      },
+    }),
     // new ConsoleLogPlugin({prefix: 'dnhyxc'})
   ],
   mode: 'development',
+  devtool: 'cheap-module-source-map',
+  devServer: {
+    port: 9012,
+    compress: true,
+    // 设置 browserHistory 路由模式时，防止出现404的情况
+    historyApiFallback: true,
+    // 不将错误信息显示在浏览器中
+    client: {
+      overlay: false,
+    },
+  }
 }
